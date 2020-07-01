58ec24503ac1954b9ff51ae99f3eb84828202c9c
Acropolis40 <arc.xoc@gmail.com>
2020-07-01 02:35:07 -0700

    Added Game over animation and reset script also created functions to simplify code

--- GameManager.gd
+++ GameManager.gd
@@ -1,7 +1,8 @@
 extends Node
 
-var balls = 4
-
+var balls = 1
+var ballsConstant = balls
+var isDead = false
 var pos = 0
 var rot = 0
 var score = 0
@@ -12,6 +13,7 @@ onready var timer = get_node("Timer")
 onready var ball_spawn = get_node("Ball Spawn")
 export (NodePath) var score_text_path 
 export (NodePath) var score_shadow_text_path 
+export (NodePath) var game_over_animator
 export (PackedScene) var particle
 export (PackedScene) var ball
 export (PackedScene) var ball_indicator
@@ -25,21 +27,7 @@ onready var score_shadow_text = get_node(score_shadow_text_path)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	timer.set_wait_time(3)
-	timer.start()
-	var i = 0
-	
-	while i < balls:
-		var ball_indicator_instance = ball_indicator.instance()
-		ball_children.append(ball_indicator_instance)
-		ball_indicator_instance.position = Vector2(50 + 40 * i,50)
-		add_child(ball_indicator_instance)
-		i += 1
-	
-		print("balls")
-		yield(get_tree(), "idle_frame")
-	print(ball_children)
-	#ball_children[0].queue_free()
+	Setup()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -60,6 +48,7 @@ func _on_Area2D_body_exited(body):
 			balls -= 1
 		else:
 			print("game over")
+			Death()
 		print(rot)
 		print(pos)
 		body.queue_free()
@@ -68,13 +57,51 @@ func _on_Area2D_body_exited(body):
 
 func _on_Timer_timeout():
 	timer.stop()
-	ball_instance = ball.instance()
-	particle_instance = particle.instance()
-	add_child(ball_instance)
+	if !isDead:
+		SpawnBall()
 	return
 	
 func Score(points):
 	score = score + points
-	score_text.bbcode_text = "[center]" + str(score) + "[/center]"
-	score_shadow_text.bbcode_text = "[center]" + str(score) + "[/center]"
+	score_text.text = str(score)
+	score_shadow_text.text = str(score)
 	print(score)
+	
+func Death():
+	isDead = true
+	get_node(game_over_animator).play("FadeOut")
+	
+func SpawnBall():
+	ball_instance = ball.instance()
+	particle_instance = particle.instance()
+	add_child(ball_instance)	
+	
+func Reset():
+	get_node(game_over_animator).play_backwards("FadeOut")
+	ball_children = []
+	isDead = false
+	score = 0
+	Score(score)
+	balls = ballsConstant
+	Setup()
+	
+func Setup():
+	timer.set_wait_time(3)
+	timer.start()
+	var i = 0
+	
+	while i < balls:
+		var ball_indicator_instance = ball_indicator.instance()
+		ball_children.append(ball_indicator_instance)
+		ball_indicator_instance.position = Vector2(50 + 40 * i,50)
+		add_child(ball_indicator_instance)
+		i += 1
+	
+		print("balls")
+		yield(get_tree(), "idle_frame")
+	print(ball_children)
+	
+
+
+func _on_TextureButton_pressed():
+	Reset()
